a256cac95442c5dbee255529c20e280c4f5e8530
christianRiv <riveramarroquin18@gmail.com>
2019-06-01 09:45:40 -0500

    botones agregados

--- examen.py
+++ examen.py
@@ -8,20 +8,47 @@ raiz=Tk()
 miFrame=Frame(raiz, width=1200, height=600)
 miFrame.pack()
 
+operacione=""
+resultado=0
+
+#
+
+numeropantalla=StringVar()
+
+
 #---------------- aca son las ventanas donde se teclea------------
 
 cuadrouno=Entry(miFrame)
 cuadrouno.grid(row=0, column=1)
 
 cuadrodos=Entry(miFrame)
-cuadrodos.grid(row=2, column=1)
+cuadrodos.grid(row=1, column=1)
+
+cuadrotres=Entry(miFrame)
+cuadrotres.grid(row=2, column=1)
 
 cuadroresultado=Entry(miFrame)
 cuadroresultado.grid(row=3, column=1)
 
-#-------------
+#-------------aca se agregara los cuadros de texto---------
+cuadrouno=Label(miFrame, text="coloca el primer numero")
+cuadrouno.grid(row=0, column=0)
 
+cuadrodos=Label(miFrame, text="coloca el segundo numero" )
+cuadrodos.grid(row=1, column=0)
 
+cuadrotres=Label(miFrame, text='coloque terer dato ')
+cuadrotres.grid(row=2, column=0)
+
+cuadroresultado=Label(miFrame, text="resulado final")
+cuadroresultado.grid(row=3, column=0)
+
+#-----------los botones------------
+boton1=Button(miFrame, text= "validacion") #validar
+boton1.grid(row=4, column=1,)
+
+boton2=Button(miFrame, text="ejecucion")#ejecuta las funcion
+boton2.grid(row=5, column=1)
 
 
 raiz.mainloop()
